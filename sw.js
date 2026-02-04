1d0386a143d4801278989dc865bc931581caf2d6
multiwasher <mariofragoso82@gmail.com>
2026-02-04 23:29:43 +0000

    Update cache version to v28

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //const CACHE_NAME = 'factory-pulse-v3';
 
 // Depois da atualização (muda para v2, v3, v4 etc.)
-const CACHE_NAME = 'factory-pulse-v27';
+const CACHE_NAME = 'factory-pulse-v28';
 
 
 const assets = [
