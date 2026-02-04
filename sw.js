5f2702a7377978e1d2a08c28de50e558ac5b0c98
multiwasher <mariofragoso82@gmail.com>
2026-02-04 19:49:21 +0000

    Mobile UI improvements: Increase button gap spacing

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //const CACHE_NAME = 'factory-pulse-v3';
 
 // Depois da atualização (muda para v2, v3, v4 etc.)
-const CACHE_NAME = 'factory-pulse-v23';
+const CACHE_NAME = 'factory-pulse-v24';
 
 
 const assets = [
