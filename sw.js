93abde396e5207ffc49a83a9d0b1d3a098497b01
multiwasher <mariofragoso82@gmail.com>
2026-02-04 17:29:29 +0000

    Update: changes in sw.js and add favicon.svg

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //const CACHE_NAME = 'factory-pulse-v3';
 
 // Depois da atualização (muda para v2, v3, v4 etc.)
-const CACHE_NAME = 'factory-pulse-v18';
+const CACHE_NAME = 'factory-pulse-v19';
 
 
 const assets = [
